b0c8b9dfd1a7dc9c0695880b52ecc4377ee0b8b7
Dee <jialiangwu4@gmail.com>
2024-09-18 22:17:31 -0500

    change upload_timestamp to auto timestamp. refine exception handling in views.file()

--- files/views.py
+++ files/views.py
@@ -1,26 +1,22 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from .models import File
 
 def index(request):
     return render(request, 'files/index.html')
 
-data = [
-    {'id': 1, 'name': 'name1.jpg'},
-    {'id': 2, 'name': 'name2.jpg'},
-    {'id': 3, 'name': 'name3.jpg'},
-    {'id': 4, 'name': 'name4.jpg'},
-]
-
-
 def files(request):
-    return render(request, 'files/files.html', {'data': data})
+    file = File.objects.all()
+    return render(request, 'files/files.html', {'data': file})
 
 
 def file(request, file_id):
-    for d in data:
-        if d.get('id')==file_id:
-            return render(request,
+    try: 
+        file = File.objects.get(pk=file_id)
+    except File.DoesNotExist:
+        raise Http404('File not found')
+        
+    return render(request,
                   'files/file.html',
-                  {'file': d}
+                  {'file': file}
                   )
-    raise Http404('No such file')
