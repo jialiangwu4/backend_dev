84aff4da56e575fc9dc9b0ece2cb1fdfcc0b1575
Dee <jialiangwu4@gmail.com>
2024-09-19 23:01:25 -0500

    added edit in views to update data in the front end

--- files/views.py
+++ files/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import File
 
 def index(request):
@@ -8,7 +8,6 @@ def index(request):
 def files(request):
     file = File.objects.all()
     return render(request, 'files/files.html', {'data': file})
-
 
 def file(request, file_id):
     try: 
@@ -20,3 +19,24 @@ def file(request, file_id):
                   'files/file.html',
                   {'file': file}
                   )
+    
+def edit(request, file_id):
+    new_name = request.POST.get('name')
+    new_type = request.POST.get('type')
+    
+    try: 
+        file = File.objects.get(pk=file_id)
+    except File.DoesNotExist:
+        raise Http404('File not found')
+    
+    # update the current file name and type 
+    if new_name:
+        file.name = new_name
+        
+    if new_type:
+        file.file_type = new_type
+    
+    # save the updated file
+    file.save()
+    
+    return redirect('files')
